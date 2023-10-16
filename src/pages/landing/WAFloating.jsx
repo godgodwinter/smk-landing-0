51a4dbfe6cf10be8f5db1dc93c112b8c28f1e1a8
godgodwinter <kakadz.pro@gmail.com>
2023-10-16 14:34:01 +0700

    update wa

--- src/pages/landing/WAFloating.jsx
+++ src/pages/landing/WAFloating.jsx
@@ -5,7 +5,7 @@ const WAFloating = () => {
   return (
     <>
       <a
-        href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
+        href="https://api.whatsapp.com/send?phone=6285259257080&text="
         class="float flex justify-center content-center tooltip"
         data-tip="Hubungi kami"
         target="_blank"
